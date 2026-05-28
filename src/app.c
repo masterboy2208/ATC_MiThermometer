ed0b29aad38b2af4be2714efe9ef502d9aae42d4
Masterboy <github@jelich.de>
2026-05-28 17:57:08 +0200

    Update app.c

--- src/app.c
+++ src/app.c
@@ -1003,12 +1003,13 @@ void main_loop(void) {
 		wrk.utc_time_sec++; // + 1 sec
 
 		// Masterboy - Switch off Advertising after 120 sec
-		if ((wrk.utc_time_sec >= 120) && (wrk.not_adv_mode == 0)) {
+		static _attribute_data_retention_ u8 adv_mode = 0;
+		if ((wrk.utc_time_sec >= 120) && (adv_mode == 0)) {
 			bls_ll_setAdvEnable(BLC_ADV_DISABLE);
-			wrk.not_adv_mode = 1; // Wir sagen der Firmware, dass das Advertising aus ist
+			adv_mode = 1;
 		}
 
-		#if (DEV_SERVICES & SERVICE_HARD_CLOCK)
+#if (DEV_SERVICES & SERVICE_HARD_CLOCK)
 		if(++rtc.seconds >= 60) {
 			rtc.seconds = 0;
 			if(++rtc.minutes >= 60) {
